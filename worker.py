818a69e72ef8c66a5ee6db0c1fbfba75feb5c0d5
Guillem Borrell <guillemborrell@gmail.com>
2019-04-18 09:36:03 +0000

    Now with a nursery working properly
    For some reason, this triggers an exception in pynng, maybe
    it is a good idea to submit a bug report to pynng with this
    exact commit

--- worker.py
+++ worker.py
@@ -2,21 +2,26 @@ from pynng import Rep0
 import trio
 import click
 
-async def reply(socket):
-    for i in range(10):
-        print('waiting for a message...')
-        message = await socket.arecv()
-        await socket.asend(b'hello back')
+async def reply(ntask, records=10):
+    with Rep0(dial='tcp://127.0.0.1:54321') as socket:
+        for i in range(records):
+            print(f'Task {ntask} waiting for a message...')
+            message = await socket.arecv()
+            await socket.asend(b'hello back')
 
     print('Collecting messages')
 
+async def parent(ntasks=4):
+    async with trio.open_nursery() as nursery:
+        for i in range(ntasks):
+            nursery.start_soon(reply, i)
+    
 @click.command()
 @click.argument('name')
 def main(name):
     print(f'Started worker {name}')
-    with Rep0(dial='tcp://127.0.0.1:54321') as socket:
-        while True:
-            trio.run(reply, socket)
+    while True:
+        trio.run(parent)
 
 if __name__ == '__main__':
     main()
