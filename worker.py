18a94ee6b3389090fd6d630bfebb26aa50c9e44e
Guillem Borrell <guillemborrell@gmail.com>
2019-04-18 09:09:33 +0000

    Now I am able to collect messages

--- worker.py
+++ worker.py
@@ -3,17 +3,20 @@ import trio
 import click
 
 async def reply(socket):
-    while True:
+    for i in range(10):
         print('waiting for a message...')
         message = await socket.arecv()
         await socket.asend(b'hello back')
+
+    print('Collecting messages')
 
 @click.command()
 @click.argument('name')
 def main(name):
     print(f'Started worker {name}')
     with Rep0(dial='tcp://127.0.0.1:54321') as socket:
-        trio.run(reply, socket)
+        while True:
+            trio.run(reply, socket)
 
 if __name__ == '__main__':
     main()
