1f064068defbf997e3d343ab9cd8950b5071ad19
Guillem Borrell <guillemborrell@gmail.com>
2019-04-18 12:25:53 +0000

    Fixed the worker implementation
    * Avoid the exception by creating the connector only once
    * Messages are collected with randomness to avoid having
      all workers writing at once

--- worker.py
+++ worker.py
@@ -1,27 +1,29 @@
 from pynng import Rep0
 import trio
 import click
+import random
 
 async def reply(ntask, records=10):
     with Rep0(dial='tcp://127.0.0.1:54321') as socket:
-        for i in range(records):
-            print(f'Task {ntask} waiting for a message...')
-            message = await socket.arecv()
-            await socket.asend(b'hello back')
+        while True:
+            for i in range(int(random.normalvariate(records, 4))):
+                print(f'Task {ntask} waiting for a message...')
+                message = await socket.arecv()
+                await socket.asend(b'hello back')
 
-    print('Collecting messages')
+            print(f'Task {ntask} collecting messages')
 
 async def parent(ntasks=4):
     async with trio.open_nursery() as nursery:
         for i in range(ntasks):
             nursery.start_soon(reply, i)
-    
+
+
 @click.command()
 @click.argument('name')
 def main(name):
     print(f'Started worker {name}')
-    while True:
-        trio.run(parent)
+    trio.run(parent)
 
 if __name__ == '__main__':
     main()
