5013e7b21b763daffcd4bc4071198834ce1fa711
Guillem Borrell <borrell.guillem@bcg.com>
2019-08-23 22:50:57 +0200

    Improved parametrization

--- flappystream-source/flappystream/source/main.py
+++ flappystream-source/flappystream/source/main.py
@@ -43,7 +43,6 @@ def build_app(backend_address="tcp://127.0.0.1", backend_port=54321):
 @click.command()
 @click.option("--host", default="0.0.0.0", help="hostname or IP")
 @click.option("--port", default=8888, help="Port where the service is run")
-@click.option("--backend_address", default="tcp://127.0.0.1", help="Address for the source nng socket")
-@click.option("--backend_port", default=54321, help="Port for the source nng socket")
-def main(host="0.0.0.0", port=8888, backend_address="tcp://127.0.0.1", backend_port=54321):
-    uvicorn.run(build_app(backend_address, backend_port), host=host, port=port)
+@click.option("--backend_url", default="tcp://127.0.0.1", help="URL for the source socket")
+def main(host="0.0.0.0", port=8888, backend_url="tcp://127.0.0.1:54321"):
+    uvicorn.run(build_app(backend_url), host=host, port=port)
--- flappystream-worker/flappystream/worker/main.py
+++ flappystream-worker/flappystream/worker/main.py
@@ -1,40 +1,40 @@
 from pynng import Pull0, Pub0, Sub0
 import click
+import ujson
 import trio
 
 
-async def hub(socket, nursery_port):
-    with Pub0(listen=f"tcp://127.0.0.1:{nursery_port}") as pub:
+async def hub(socket, nursery_url):
+    with Pub0(listen=nursery_url) as pub:
         while True:
             log = await socket.arecv()
             await pub.asend(log)
 
 
-async def echo(nursery_port):
-    with Sub0(dial=f"tcp://127.0.0.1:{nursery_port}") as sub:
+async def echo(nursery_url):
+    with Sub0(dial=nursery_url) as sub:
         sub.subscribe(b"")  # Subscribe to everything
         while True:
-            log = await sub.arecv()
+            log = ujson.loads(await sub.arecv())
 
 
-async def parent(socket, nursery_port):
+async def parent(socket, nursery_url):
     async with trio.open_nursery() as nursery:
-        nursery.start_soon(echo, nursery_port)
-        nursery.start_soon(hub, socket, nursery_port)
+        nursery.start_soon(echo, nursery_url)
+        nursery.start_soon(hub, socket, nursery_url)
 
 
 @click.command()
 @click.option(
-    "--backend_address",
-    default="tcp://127.0.0.1",
-    help="Address to dial for the source socket",
+    "--backend_url",
+    default="tcp://127.0.0.1:54321",
+    help="URL to dial for the source socket",
 )
-@click.option("--backend_port", default=54321, help="Port fo the source socket")
 @click.option(
-    "--nursery_port",
-    default=54322,
-    help="Port to be used to broadcast the message to all the workers",
+    "--nursery_url",
+    default="tcp://127.0.0.1:54322",
+    help="URL for the internal PUB-SUB queue",
 )
-def main(backend_address, backend_port, nursery_port):
-    with Pull0(dial=f"{backend_address}:{backend_port}") as socket:
-        trio.run(parent, socket, nursery_port)
+def main(backend_url, nursery_url):
+    with Pull0(dial=backend_url) as socket:
+        trio.run(parent, socket, nursery_url)
