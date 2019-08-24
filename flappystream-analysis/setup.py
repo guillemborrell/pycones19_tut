9592d9f9ff3359ca73df559ceee682ab728158c5
Guillem Borrell <borrell.guillem@bcg.com>
2019-08-24 11:58:56 +0200

    Improved dependency management

--- flappystream-analysis/setup.py
+++ flappystream-analysis/setup.py
@@ -6,7 +6,7 @@ setup(
     author_email="guillemborrell@gmail.com",
     packages=find_namespace_packages(),
     description="Source server for the flappystream application",
-    install_requires=["toolz", "pytest-asyncio"],
+    install_requires=["pytest-asyncio"],
     entry_points={"console_scripts":
                   ['flappystream-front = flappystream.source.main:main']},
     include_package_files=True
